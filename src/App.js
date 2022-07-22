c539483fc75e9402d7b5038c1e4f8fc0fb337573
suryamani1kumar <8437suryamanikumar@gmail.com>
2022-07-22 22:07:32 +0530

    fix:add searchvideo page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ import Youtubehomepage from './component/Youtubehomepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WatchVideo from './page/WatchVideo';
 import SearchVideo from './page/SearchVideo';
+import { ThemeProvider } from '@mui/material';
+import theme from './assets/theme';
+
 
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Youtubehomepage />} />
-          <Route path='/watchvideo' element={<WatchVideo />} />
-          <Route path='/searchvideo' element={<SearchVideo/>} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Youtubehomepage />} />
+            <Route path='/watchvideo' element={<WatchVideo />} />
+            <Route path='/searchvideo' element={<SearchVideo />} />
+          </Routes>
+        </ThemeProvider>
       </Provider>
     </div>
   )
